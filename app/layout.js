4ab3f01514640bdb4b85d9155badb13aa22833ce
shourya mishra <shourya.desk@gmail.com>
2024-11-11 11:24:16 +0530

    added tags

--- app/layout.js
+++ app/layout.js
@@ -35,6 +35,82 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GQJ4EGC6QM"></script>
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GQJ4EGC6QM');
+          `,
+        }}
+      />
+
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-T4M7X33H"
+          height="0" 
+          width="0" 
+          style={{ display: "none", visibility: "hidden" } }
+        > 
+        </iframe>
+      </noscript>
+
+      <script 
+        dangerouslySetInnerHTML={{
+          __html: `
+          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T4M7X33H'
+          `
+        }}
+      />
+
+      <script 
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "omgfxv9mki"
+          `
+        }}
+      />
+
+      <script 
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3520646018188374');
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=3520646018188374&ev=PageView&noscript=1"
+        />
+      </noscript>
+
+      </head>
       <body
         className={`${poppins.variable} ${helveticaNow.variable} ${TTChocolates.variable} font-tt-chocolates antialiased`}
       >
